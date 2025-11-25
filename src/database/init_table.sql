01986718500ceb9717cee57fd79f2e907a6b24e5
Trickderap <10423114@student.vgu.edu.vn>
2025-11-25 20:24:57 +0700

    Update init_table.sql
    None

--- src/database/init_table.sql
+++ src/database/init_table.sql
@@ -1,95 +1,96 @@
+CREATE DATABASE  IF NOT EXISTS `class_management` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `class_management`;
 -- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
 --
--- Host: localhost    Database: classmanager
+-- Host: localhost    Database: class_management
 -- ------------------------------------------------------
 -- Server version	8.0.44
---
--- Table structure for table `classes`
---
 
-DROP TABLE IF EXISTS `classes`;
-CREATE TABLE `classes` (
-  `ClassID` int NOT NULL AUTO_INCREMENT,
-  `CourseID` int NOT NULL,
-  `InstructorID` int NOT NULL,
-  `ClassTime` time NOT NULL,
-  `Semester` varchar(20) NOT NULL,
-  PRIMARY KEY (`ClassID`),
-  KEY `CourseID` (`CourseID`),
-  KEY `InstructorID` (`InstructorID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `courses`
 --
 
 DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `courses` (
   `CourseID` int NOT NULL AUTO_INCREMENT,
   `CourseName` varchar(100) NOT NULL,
   `CourseCode` varchar(10) NOT NULL,
   `Description` text,
   `Credits` decimal(3,2) DEFAULT NULL,
+  `RequiresLab` tinyint(1) DEFAULT NULL,
+  `SessionCount` int DEFAULT NULL,
+  `EnrollmentLimit` int DEFAULT NULL,
+  `IsActive` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`CourseID`),
   UNIQUE KEY `CourseCode` (`CourseCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `enrollments`
 --
 
 DROP TABLE IF EXISTS `enrollments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `enrollments` (
   `EnrollmentID` int NOT NULL AUTO_INCREMENT,
   `StudentID` int NOT NULL,
-  `ClassID` int NOT NULL,
   `EnrollmentDate` date NOT NULL,
   `IsActive` bit(1) DEFAULT NULL,
+  `CourseID` int NOT NULL,
+  `InstructorID` int NOT NULL,
+  `Semester` varchar(20) NOT NULL,
   PRIMARY KEY (`EnrollmentID`),
-  KEY `FK_Enrollments_Student` (`StudentID`),
-  KEY `FK_Enrollments_Class` (`ClassID`),
-  CONSTRAINT `FK_Enrollments_Class` FOREIGN KEY (`ClassID`) REFERENCES `classes` (`ClassID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_Enrollments_Student` FOREIGN KEY (`StudentID`) REFERENCES `students` (`StudentID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `StudentID` (`StudentID`),
+  KEY `CourseID` (`CourseID`),
+  KEY `InstructorID` (`InstructorID`),
+  CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`StudentID`) REFERENCES `students` (`StudentID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`CourseID`) REFERENCES `courses` (`CourseID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `enrollments_ibfk_3` FOREIGN KEY (`InstructorID`) REFERENCES `instructors` (`InstructorID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `instructors`
 --
 
 DROP TABLE IF EXISTS `instructors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `instructors` (
   `InstructorID` int NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(50) NOT NULL,
   `LastName` varchar(50) NOT NULL,
   `HireDate` date DEFAULT NULL,
   `Department` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`InstructorID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Table structure for table `labsessions`
---
-
-DROP TABLE IF EXISTS `labsessions`;
-CREATE TABLE `labsessions` (
-  `LabSessionID` int NOT NULL AUTO_INCREMENT,
-  `ClassID` int NOT NULL,
-  `RoomID` int NOT NULL,
-  `SessionDate` date NOT NULL,
-  `StartTime` time NOT NULL,
-  `EndTime` time NOT NULL,
-  PRIMARY KEY (`LabSessionID`),
-  KEY `ClassID` (`ClassID`),
-  KEY `FK_LabSessions_Room` (`RoomID`),
-  CONSTRAINT `FK_LabSessions_Room` FOREIGN KEY (`RoomID`) REFERENCES `rooms` (`RoomID`),
-  CONSTRAINT `labsessions_ibfk_1` FOREIGN KEY (`ClassID`) REFERENCES `classes` (`ClassID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Phone` varchar(20) DEFAULT NULL,
+  `Email` varchar(100) NOT NULL,
+  PRIMARY KEY (`InstructorID`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `rooms`
 --
 
 DROP TABLE IF EXISTS `rooms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rooms` (
   `RoomID` int NOT NULL AUTO_INCREMENT,
   `RoomNumber` varchar(10) NOT NULL,
@@ -97,13 +98,69 @@ CREATE TABLE `rooms` (
   `RoomType` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`RoomID`),
   UNIQUE KEY `RoomNumber` (`RoomNumber`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `schedules`
+--
+
+DROP TABLE IF EXISTS `schedules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `schedules` (
+  `ClassID` int NOT NULL AUTO_INCREMENT,
+  `CourseID` int NOT NULL,
+  `InstructorID` int NOT NULL,
+  `SessionType` varchar(20) NOT NULL,
+  `StartDate` date NOT NULL,
+  `EndDate` date NOT NULL,
+  `RoomID` int NOT NULL,
+  `DayOfWeek` varchar(10) NOT NULL,
+  `StartTime` time NOT NULL,
+  `EndTime` time NOT NULL,
+  `Semester` varchar(20) NOT NULL,
+  PRIMARY KEY (`ClassID`),
+  KEY `CourseID` (`CourseID`),
+  KEY `InstructorID` (`InstructorID`),
+  KEY `RoomID` (`RoomID`),
+  CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`CourseID`) REFERENCES `courses` (`CourseID`) ON DELETE CASCADE,
+  CONSTRAINT `schedules_ibfk_2` FOREIGN KEY (`InstructorID`) REFERENCES `instructors` (`InstructorID`) ON DELETE CASCADE,
+  CONSTRAINT `schedules_ibfk_4` FOREIGN KEY (`RoomID`) REFERENCES `rooms` (`RoomID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `schedulesexceptions`
+--
+
+DROP TABLE IF EXISTS `schedulesexceptions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `schedulesexceptions` (
+  `ExceptionID` int NOT NULL AUTO_INCREMENT,
+  `ClassID` int NOT NULL,
+  `ExceptionDate` date NOT NULL,
+  `ClassStatus` varchar(20) NOT NULL,
+  `NewDate` date DEFAULT NULL,
+  `StartTime` time DEFAULT NULL,
+  `EndTime` time DEFAULT NULL,
+  `RoomID` int DEFAULT NULL,
+  PRIMARY KEY (`ExceptionID`),
+  KEY `ClassID` (`ClassID`),
+  KEY `RoomID` (`RoomID`),
+  CONSTRAINT `schedulesexceptions_ibfk_1` FOREIGN KEY (`ClassID`) REFERENCES `schedules` (`ClassID`) ON DELETE CASCADE,
+  CONSTRAINT `schedulesexceptions_ibfk_2` FOREIGN KEY (`RoomID`) REFERENCES `rooms` (`RoomID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `students`
 --
 
 DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `students` (
   `StudentID` int NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(50) NOT NULL,
@@ -113,6 +170,16 @@ CREATE TABLE `students` (
   `Phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`StudentID`),
   UNIQUE KEY `Email` (`Email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- Dump completed on 2025-11-21 21:48:09
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-25 20:23:18
